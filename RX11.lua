c970580de8b1ae16adc1146c0c201919c49a56e3
marjorie <111649405+marjoriex@users.noreply.github.com>
2023-02-12 21:45:59 +0000

    Update RX11.lua

--- RX11.lua
+++ RX11.lua
@@ -37,6 +37,7 @@ function RX11:draw_string(string_content, int_x, int_y, color3_color)
     RX11_STRING.Font = 0
     RX11_STRING.Text = string_content
     RX11_STRING.Position = Vector2.new(int_x, int_y)
+    RX11_STRING.Color = color3_color
 
     table.insert(RX11.RX11_RENDERED, RX11_STRING)
     return RX11_STRING
@@ -52,6 +53,7 @@ function RX11:draw_string_outline(string_content, int_x, int_y, color3_color, co
     RX11_STRING_OUTLINE.Text = string_content
     RX11_STRING_OUTLINE.OutlineColor = color3_outline_color
     RX11_STRING_OUTLINE.Position = Vector2.new(int_x, int_y)
+    RX11_STRING_OUTLINE.Color = color3_color
 
     table.insert(RX11.RX11_RENDERED, RX11_STRING_OUTLINE)
     return RX11_STRING_OUTLINE
@@ -66,6 +68,7 @@ function RX11:draw_string_centered(string_content, int_x, int_y, color3_color)
     RX11_STRING_CENTERED.Font = 0
     RX11_STRING_CENTERED.Text = string_content
     RX11_STRING_CENTERED.Position = Vector2.new(int_x, int_y)
+    RX11_STRING_CENTERED.Color = color3_color
 
     table.insert(RX11.RX11_RENDERED, RX11_STRING_CENTERED)
     return RX11_STRING_CENTERED
